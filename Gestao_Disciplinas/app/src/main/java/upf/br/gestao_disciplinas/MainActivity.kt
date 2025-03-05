ecd8d51a08c7a4b41171c5526e42f1b98a33e6e5
Bruno Pressi <brunopressi2012@gmail.com>
2025-03-05 19:54:49 -0300

    projeto aula dev mobile 2

---
+++ Gestao_Disciplinas/app/src/main/java/upf/br/gestao_disciplinas/MainActivity.kt
@@ -0,0 +1,47 @@
+package upf.br.gestao_disciplinas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import upf.br.gestao_disciplinas.ui.theme.Gestao_DisciplinasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Gestao_DisciplinasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Gestao_DisciplinasTheme {
+        Greeting("Android")
+    }
+}
